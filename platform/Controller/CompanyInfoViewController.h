f5fde48a4d9d1c78e1cd5e550554bf0565427e00
chliu.brook <liuchunhua@shhxzq.com>
2017-06-27 07:44:17 +0800

    commit

---
+++ platform/Controller/CompanyInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompanyInfoViewController.h
+//  platform
+//
+//  Created by chliu.brook on 27/06/2017.
+//  Copyright © 2017 chliu.brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompanyInfoViewController : UIViewController
+
+@end
---
+++ platform/Controller/MyCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyCenterViewController.h
+//  platform
+//
+//  Created by chliu.brook on 27/06/2017.
+//  Copyright © 2017 chliu.brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCenterViewController : UIViewController
+
+@end
