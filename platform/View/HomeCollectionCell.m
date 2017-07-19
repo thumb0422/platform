581dec668d2feaeb8ac216933cda018a3a2be797
liuchunhua <thumb0422@163.com>
2017-07-19 22:03:02 +0800

    修改图片布局
    修改图片布局

--- platform/View/HomeCollectionCell.m
+++ platform/View/HomeCollectionCell.m
@@ -17,7 +17,8 @@
     self.layer.cornerRadius = 4;
     self.layer.borderWidth = 1;
     self.layer.borderColor = [UIColor clearColor].CGColor;
-    self.imageView.layer.cornerRadius = self.imageView.frame.size.width / 2;
+//    self.imageView.layer.cornerRadius = self.imageView.frame.size.width / 2;
+    self.imageView.layer.cornerRadius = 4;
 }
 
 @end
