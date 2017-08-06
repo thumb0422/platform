48caa33769bb8910761834162a5170420c71eb9f
liuchunhua <thumb0422@163.com>
2017-08-07 07:17:41 +0800

    commit
    commit

---
+++ platform/Model/AccountModel.m
@@ -0,0 +1,13 @@
+//
+//  AccountModel.m
+//  platform
+//
+//  Created by chliu.brook on 06/08/2017.
+//  Copyright © 2017 chliu.brook. All rights reserved.
+//
+
+#import "AccountModel.h"
+
+@implementation AccountModel
+
+@end
