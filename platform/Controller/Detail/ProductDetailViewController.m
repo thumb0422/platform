ab6f486cd81f4366ba2fb2e0290ae0b2051b3c9c
liuchunhua <thumb0422@163.com>
2017-08-16 07:23:43 +0800

    update
    update

--- platform/Controller/Detail/ProductDetailViewController.m
+++ platform/Controller/Detail/ProductDetailViewController.m
@@ -16,6 +16,7 @@
 @property (nonatomic,strong) UIView *centerView;
 @property (nonatomic,strong) UIView *bottomView;
 @property (nonatomic,strong) UIView *buyView;
+@property (nonatomic,strong) HMSegmentedControl *segmentCtrl;
 @end
 
 @implementation ProductDetailViewController
@@ -23,28 +24,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initNav];
-    self.view.backgroundColor = [UIColor whiteColor];
+    [self.navigationController.navigationBar lt_setBackgroundColor:[UIColor clearColor]];
+    self.view.backgroundColor = [UIColor clearColor];
     [self.view addSubview:self.bgScrollView];
     [self initUI];
     [self.view addSubview:self.buyView];
-    [self.navigationController.navigationBar lt_setBackgroundColor:[UIColor clearColor]];
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    UIColor * color = [UIColor colorWithRed:0/255.0 green:175/255.0 blue:240/255.0 alpha:1];
-    CGFloat offsetY = scrollView.contentOffset.y;
-    if (offsetY > NAVBAR_CHANGE_POINT) {
-        CGFloat alpha = MIN(1, 1 - ((NAVBAR_CHANGE_POINT + 64 - offsetY) / 64));
-        [self.navigationController.navigationBar lt_setBackgroundColor:[color colorWithAlphaComponent:alpha]];
-    } else {
-        [self.navigationController.navigationBar lt_setBackgroundColor:[color colorWithAlphaComponent:0]];
-    }
 }
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
 }
 
 - (void)viewDidDisappear:(BOOL)animated{
@@ -60,8 +48,6 @@
 - (void)initNav{
     UIBarButtonItem *leftBar = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back"] style:UIBarButtonItemStyleDone target:self action:@selector(leftBarClick)];
     self.navigationItem.leftBarButtonItem = leftBar;
-    
-    self.navigationItem.title = @"";
 }
 
 - (void)leftBarClick{
@@ -82,6 +68,14 @@
         make.top.equalTo(self.centerView.mas_bottom).offset(10);
         make.height.equalTo(@600);
     }];
+    [self.centerView addSubview:self.segmentCtrl];
+    [self.segmentCtrl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(@0);
+        make.width.equalTo(self.centerView.mas_width);
+        make.top.equalTo(@0);
+        make.height.equalTo(self.centerView.mas_height).offset(-5);
+    }];
+    [self loadBottomSubView];
 }
 
 - (UIScrollView *)bgScrollView{
@@ -96,8 +90,25 @@
 
 - (UIView *)topView{
     if (!_topView){
-        _topView = [[UIView alloc] initWithFrame:CGRectMake(0, - NAV_STATUS_HEIGHT, SCREEN_WIDTH, 150)];
-        _topView.backgroundColor = [UIColor grayColor];
+        _topView = [[UIView alloc] initWithFrame:CGRectMake(0, - NAV_STATUS_HEIGHT, SCREEN_WIDTH, 200)];
+        _topView.backgroundColor = [UIColor clearColor];
+        
+        NSArray * topBannerArray = @[@"topBanner1.jpg",@"topBanner2.jpg",@"topBanner1.jpg",@"topBanner2.jpg"];
+        SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, _topView.width, _topView.height - 50) shouldInfiniteLoop:YES imageNamesGroup:topBannerArray];
+        cycleScrollView.pageControlStyle = SDCycleScrollViewPageContolStyleAnimated;
+        cycleScrollView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        [_topView addSubview:cycleScrollView];
+        
+        UIView *descView = [[UIView alloc] initWithFrame:CGRectMake(0, cycleScrollView.height + 5, _topView.width, _topView.height - (cycleScrollView.height + 5))];
+        descView.backgroundColor = [UIColor whiteColor];
+        UILabel *descLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,0, descView.width, descView.height)];
+        descLabel.text = @"这是一个测试用例，这是一个马桶，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机，这是一个洗衣机,这是一个测试用例，这是一个马桶，这是一个洗衣机";
+        descLabel.textColor = [UIColor blackColor];
+        descLabel.font = [UIFont systemFontOfSize:14];
+        descLabel.numberOfLines = 0;
+        descLabel.adjustsFontSizeToFitWidth = YES;
+        [descView addSubview:descLabel];
+        [_topView addSubview:descView];
     }
     return _topView;
 }
@@ -105,17 +116,57 @@
 - (UIView *)centerView{
     if (!_centerView){
         _centerView = [[UIView alloc] init];
-        _centerView.backgroundColor = [UIColor yellowColor];
+        _centerView.backgroundColor = [UIColor clearColor];
     }
     return _centerView;
+}
+
+- (HMSegmentedControl *)segmentCtrl{
+    if (!_segmentCtrl){
+        _segmentCtrl = [[HMSegmentedControl alloc] init];
+        _segmentCtrl.sectionTitles = @[@"商品参数",@"商品详情"];
+        _segmentCtrl.selectedSegmentIndex = 0;
+        _segmentCtrl.backgroundColor = [UIColor colorWithRed:0.7 green:0.7 blue:0.7 alpha:1];
+        _segmentCtrl.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+        _segmentCtrl.selectedTitleTextAttributes = @{NSForegroundColorAttributeName : [UIColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:1]};
+        _segmentCtrl.selectionIndicatorColor = [UIColor colorWithRed:0.3 green:0.3 blue:0.3 alpha:1];
+        _segmentCtrl.selectionStyle = HMSegmentedControlSelectionStyleBox;
+        _segmentCtrl.selectionIndicatorLocation = HMSegmentedControlSelectionIndicatorLocationDown;
+    }
+    return _segmentCtrl;
 }
 
 - (UIView *)bottomView{
     if (!_bottomView){
         _bottomView = [[UIView alloc] init];
-        _bottomView.backgroundColor = [UIColor orangeColor];
+        _bottomView.backgroundColor = [UIColor clearColor];
     }
     return _bottomView;
+}
+
+- (void)loadBottomSubView{
+    UIImageView *imgV1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"information"]];
+    imgV1.contentMode = UIViewContentModeScaleAspectFit;
+    [self.bottomView addSubview:imgV1];
+    
+    UIImageView *imgV2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"instructions"]];
+    imgV2.contentMode = UIViewContentModeScaleAspectFit;
+    [self.bottomView addSubview:imgV2];
+    
+    __weak typeof(self) weakSelf = self;
+    [self.segmentCtrl setIndexChangeBlock:^(NSInteger index) {
+        if (index == 0){
+            [imgV2 setHidden:YES];
+            [imgV1 setHidden:NO];
+            [weakSelf.bottomView setFrame:CGRectMake(weakSelf.bottomView.left, weakSelf.bottomView.top, weakSelf.bottomView.width, imgV1.height)];
+            [weakSelf.bgScrollView setContentSize:CGSizeMake(SCREEN_WIDTH, weakSelf.bottomView.bottom + NAV_HEIGHT)];
+        }else {
+            [imgV1 setHidden:YES];
+            [imgV2 setHidden:NO];
+            [weakSelf.bottomView setFrame:CGRectMake(weakSelf.bottomView.left, weakSelf.bottomView.top, weakSelf.bottomView.width, imgV2.height)];
+            [weakSelf.bgScrollView setContentSize:CGSizeMake(SCREEN_WIDTH, weakSelf.bottomView.bottom + NAV_HEIGHT)];
+        }
+    }];
 }
 
 - (UIView *)buyView{
@@ -160,6 +211,10 @@
 }
 
 - (void)collectEvent{
+    
+}
+
+- (void) segmentChanged:(UISegmentedControl *)sender{
     
 }
 
